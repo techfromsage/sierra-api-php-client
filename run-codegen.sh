e57d311f04365292b60005b605bcc6a1bb659261
Tim Hodson <tim@timhodson.com>
2017-11-24 15:01:02 +0000

    add swagger tool to repo

--- run-codegen.sh
+++ run-codegen.sh
@@ -4,7 +4,7 @@ echo -e "Removing current files\n\n"
 rm -rf ./Sierra ./docs ./test ./composer.* ./phpunit.xml.dist ./git_push.sh ./README.md ./.travis.yml ./.swagger-codegen/
 
 echo -e "Rebuilding\n\n"
-java -jar ~/Downloads/swagger-codegen-cli-3.0.0-20171123.081951-8.jar\
+java -jar ./swagger-codegen-cli-3.0.0-20171123.081951-8.jar\
  generate\
  -l php\
  -i https://sandbox.iii.com/iii/sierra-api/docs\
